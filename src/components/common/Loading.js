289a67c0354cc474f7cded56b70f7d56c9e15107
F.Achilles <bluekobear@gmail.com>
2017-10-11 22:32:54 +0800

    Did add loading component...

---
+++ src/components/common/Loading.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View } from 'react-native';
+
+export default class Loading extends React.Component {
+    render() {
+        return (
+            <View>
+                Loading ...
+            </View>
+        )
+    }
+}
